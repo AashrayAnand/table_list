918273aa7ea901d45864d209efdcd3365578a375
Ozan Erdal <oerdal@Ozans-MacBook-Pro.local>
2018-10-21 14:34:04 -0700

    added more time functionality to bookings

--- frontend/src/components/ScheduleItem.js
+++ frontend/src/components/ScheduleItem.js
@@ -5,19 +5,28 @@ const ScheduleItem = ({data}) => (
         <div className="card-body">
             <h3 className="card-title">{data.location}</h3>
             <ul className="m-0">
-                <li>{dateTime(new Date(data.date))}</li>
+                <li>{convDate(new Date(data.date))}</li>
+                <li>{convTime(new Date(data.start_date))} - {convTime(new Date(data.end_date))}</li>
             </ul>
         </div>
     </div> 
 );
 
-const dateTime = (d) => {
-    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+const convDate = (d) => {
     var month = months[d.getUTCMonth()];
     var day = d.getUTCDate();
     var year = d.getUTCFullYear();
-    var time = "" + d.getUTCHours() + ":" + (d.getUTCMinutes()==0?"00":"30") + (d.getUTCHours()>=12?"PM":"AM");
-    return (month + " " + day + " " + year + " - " + time);
+    return (month + " " + day + " " + year);
+}
+
+const convTime = (d) => {
+    var hours = d.getUTCHours();
+    var pm = hours > 12;
+    hours = (pm ? hours - 12 : hours);
+    var time = hours + ":" + (d.getUTCMinutes() == 0 ? "00" : "30") + (pm ? "PM" : "AM");
+    return (time);
 }
 
 export default ScheduleItem;
