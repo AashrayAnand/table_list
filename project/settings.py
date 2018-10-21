664eefb7e122d7c804d3157bca4be5009363e57a
Aashray <aashray@uw.edu>
2018-10-21 13:02:20 -0700

    removed heroku bullshit from settings

--- project/settings.py
+++ project/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -24,7 +23,6 @@ SECRET_KEY = 'hm)5&08^5_^jhuno=+y&r&khnxbsb_3jps!yt15741)b&*0o&m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# temporary settings, following heroku postgrest tutorial
 ALLOWED_HOSTS = []
 
 
