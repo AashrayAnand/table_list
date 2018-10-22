a4baee2c61982ff694154fb93c5998e6e84ee98e
Aashray <aashray@uw.edu>
2018-10-21 18:02:37 -0700

    asd

--- project/settings.py
+++ project/settings.py
@@ -19,7 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'hm)5&08^5_^jhuno=+y&r&khnxbsb_3jps!yt15741)b&*0o&m'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
