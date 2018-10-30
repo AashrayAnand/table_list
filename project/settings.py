28193d74df3dda393211490a4b19302aa6abe2bf
Aashray <aashray@uw.edu>
2018-10-30 00:36:03 -0700

    ads

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['live.uwpaphi.site']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
